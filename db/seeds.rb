5fe4c2df017e458e694fa1d0e7a94df18ef74fdd
samjbruce <samueljamesbruce@gmail.com>
2021-07-30 10:30:30 -0500

    updates gems

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-# User.create!([
-#   {name: "Sam Bruce", email: "sam@gmail.com", password_digest: "password", image_url: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.7ZXp-OfHU7muAT57NsBMLQAAAA%26pid%3DApi&f=1"},
+User.create!([
+  {name: "Sam Bruce", email: "sam@gmail.com", password_digest: "password", image_url: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.7ZXp-OfHU7muAT57NsBMLQAAAA%26pid%3DApi&f=1"},
 #   {name: "Katie Sayres", email: "katie@gmail.com", password_digest: "password", image_url: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.wXPPy5Odf4f4b_9G0hH4zAHaFd%26pid%3DApi&f=1"},
 #   {name: "Joe Brown", email: "joe@gmail.com", password_digest: "password", image_url: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.oh3qPvA_BH-2kakEJPVN-AAAAA%26pid%3DApi&f=1"}
 #   {name: "Jake Simpson", email: "jake@gmail.com", password_digest: "password", image_url: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.oh3qPvA_BH-2kakEJPVN-AAAAA%26pid%3DApi&f=1"}
@@ -22,12 +22,12 @@
 #   {name: "lemons", user_id: 10, have: false},
 # ])
 
-Favorite.create!([
-  {spoonacular_api_id: 602643, user_id: 10},
-  {spoonacular_api_id: 638716, user_id: 10},
-  {spoonacular_api_id: 641901, user_id: 10},
-  {spoonacular_api_id: 715525, user_id: 12},
-  {spoonacular_api_id: 800780, user_id: 12},
-  {spoonacular_api_id: 638717, user_id: 12}
+# Favorite.create!([
+#   {spoonacular_api_id: 602643, user_id: 10},
+#   {spoonacular_api_id: 638716, user_id: 10},
+#   {spoonacular_api_id: 641901, user_id: 10},
+#   {spoonacular_api_id: 715525, user_id: 12},
+#   {spoonacular_api_id: 800780, user_id: 12},
+#   {spoonacular_api_id: 638717, user_id: 12}
 
-])
+# ])
